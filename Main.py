e808171184942c092f9a68a20bec4bdf219940cc
Plasma-Vortex <howardhalim@gmail.com>
2019-04-15 01:04:18 -0400

    Trained NNs more

--- Main.py
+++ Main.py
@@ -72,9 +72,9 @@ s20_10 = [-28, -5, -10, -16, -15, -4, -20, -16, -27, -26, -22, -14, 1, 8, 13, 5,
 # plt.show()
 
 # n = Net("128-128-128-128", 2980)
-# n = Net('Conv v2', 11720)
-# n = Net('ResNet v1', 3390)
-n = Net('ResNet v2', 0)
+# n = Net('Conv v2', 15000)
+# n = Net('ResNet v1', 5440)
+n = Net('ResNet v2', 2470)
 
 n.train(5, 50)
 # n.playHuman(100, 0)
--- RemoveFiles.py
+++ RemoveFiles.py
@@ -1,10 +1,12 @@
 import os
 
-NN_name = 'ResNet v1'
+NN_name = 'Conv v2'
 files = os.listdir('NNs/'+NN_name)
 ages = [int(f[len(NN_name)+2:-3]) for f in files]
 maxAge = max(ages)
+removed = 0
 for i in ages:
-    if i%50 != 0 and i < maxAge:
+    if i%100 != 0 and i < maxAge:
         os.remove('NNs/' + NN_name + '/' + NN_name + ', ' + str(i) + '.h5')
-print('done')
+        removed += 1
+print('%d files removed' % removed)
